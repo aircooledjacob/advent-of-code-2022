9dc526af984cbb23ee1179acd18d24a4917fbc7b
Jacob Neave <35272149+aircooledjacob@users.noreply.github.com>
2023-02-25 20:18:38 +0000

    finished day 2 p1
    added code to sum up score

--- src/bin/d2.rs
+++ src/bin/d2.rs
@@ -20,7 +20,6 @@ fn main() {
         .iter()
         .map(|line| {
             let split_line = line.split_once(' ').expect("Malformed input");
-
             (
                 split_line.0.chars().collect::<Vec<char>>()[0],
                 split_line.1.chars().collect::<Vec<char>>()[0],
@@ -28,5 +27,30 @@ fn main() {
         })
         .collect();
 
-    println!("{tuple_vec:?}")
+    let mut score: u32 = 0;
+    for (opponent_choice, your_choice) in tuple_vec {
+        //increase score for outcome
+        match (opponent_choice, your_choice) {
+            ('A', 'X') => score += 3,
+            ('A', 'Y') => score += 6,
+            ('A', 'Z') => score += 0,
+            ('B', 'X') => score += 0,
+            ('B', 'Y') => score += 3,
+            ('B', 'Z') => score += 6,
+            ('C', 'X') => score += 6,
+            ('C', 'Y') => score += 0,
+            ('C', 'Z') => score += 3,
+            _ => panic!("Unexpected value in first column, acceptable values are: A, B, C"),
+        }
+
+        //increase score for choice
+        match your_choice {
+            'X' => score += 1,
+            'Y' => score += 2,
+            'Z' => score += 3,
+            _ => panic!("unexpected value in second column, acceptable values are: X, Y, Z"),
+        }
+    }
+
+    println!("Your final score is {score}")
 }

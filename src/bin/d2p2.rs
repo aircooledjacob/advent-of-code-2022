56882120e7d29c2e20f632d1122227387a08ff83
Jacob Neave <35272149+aircooledjacob@users.noreply.github.com>
2023-02-26 10:49:45 +0000

    finished d2p2
    d2p2 now functions and returns the correct result

--- src/bin/d2p2.rs
+++ src/bin/d2p2.rs
@@ -16,5 +16,27 @@ fn main() {
 
     let raw_lines: Vec<String> = common::read_file_to_vec_of_strings(&args.filename);
 
-    let tuple_vec = common::d2::parse_lines_to_char_tuples(&raw_lines);
+    let input_tuple_vec = common::d2::parse_lines_to_char_tuples(&raw_lines);
+
+    let converted_tuple_vec: Vec<(char, char)> = input_tuple_vec
+        .iter()
+        .map(
+            |(opponent_choice, outcome)| match (opponent_choice, outcome) {
+                ('A', 'X') => ('A', 'Z'),
+                ('A', 'Y') => ('A', 'X'),
+                ('A', 'Z') => ('A', 'Y'),
+                ('B', 'X') => ('B', 'X'),
+                ('B', 'Y') => ('B', 'Y'),
+                ('B', 'Z') => ('B', 'Z'),
+                ('C', 'X') => ('C', 'Y'),
+                ('C', 'Y') => ('C', 'Z'),
+                ('C', 'Z') => ('C', 'X'),
+                _ => panic!("Unexpected char in file."),
+            },
+        )
+        .collect();
+
+    let score = common::d2::calculate_score(&converted_tuple_vec);
+
+    println!("Your final score is {score}")
 }

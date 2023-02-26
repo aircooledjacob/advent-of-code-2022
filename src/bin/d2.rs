2b733fefb96d3881c4073e99a449c4bc9a8f1d01
Jacob Neave <35272149+aircooledjacob@users.noreply.github.com>
2023-02-26 10:49:20 +0000

    reduce code duplication
    create common function for calculating score of rps game from two choices
    this function will also be needed in d2p2. moving so there's no duplication.

--- src/bin/d2.rs
+++ src/bin/d2.rs
@@ -18,30 +18,7 @@ fn main() {
 
     let tuple_vec = common::d2::parse_lines_to_char_tuples(&raw_lines);
 
-    let mut score: u32 = 0;
-    for (opponent_choice, your_choice) in tuple_vec {
-        //increase score for outcome
-        match (opponent_choice, your_choice) {
-            ('A', 'X') => score += 3,
-            ('A', 'Y') => score += 6,
-            ('A', 'Z') => score += 0,
-            ('B', 'X') => score += 0,
-            ('B', 'Y') => score += 3,
-            ('B', 'Z') => score += 6,
-            ('C', 'X') => score += 6,
-            ('C', 'Y') => score += 0,
-            ('C', 'Z') => score += 3,
-            _ => panic!("Unexpected value in first column, acceptable values are: A, B, C"),
-        }
-
-        //increase score for choice
-        match your_choice {
-            'X' => score += 1,
-            'Y' => score += 2,
-            'Z' => score += 3,
-            _ => panic!("unexpected value in second column, acceptable values are: X, Y, Z"),
-        }
-    }
+    let score = common::d2::calculate_score(&tuple_vec);
 
     println!("Your final score is {score}")
 }
--- src/common/d2.rs
+++ src/common/d2.rs
@@ -10,3 +10,31 @@ pub fn parse_lines_to_char_tuples(raw_lines_as_string: &Vec<String>) -> Vec<(cha
         })
         .collect()
 }
+
+pub fn calculate_score(moves_tuple_vec: &Vec<(char, char)>) -> u32 {
+    let mut score: u32 = 0;
+    for (opponent_choice, your_choice) in moves_tuple_vec {
+        //increase score for outcome
+        match (opponent_choice, your_choice) {
+            ('A', 'X') => score += 3,
+            ('A', 'Y') => score += 6,
+            ('A', 'Z') => score += 0,
+            ('B', 'X') => score += 0,
+            ('B', 'Y') => score += 3,
+            ('B', 'Z') => score += 6,
+            ('C', 'X') => score += 6,
+            ('C', 'Y') => score += 0,
+            ('C', 'Z') => score += 3,
+            _ => panic!("Unexpected value in first column, acceptable values are: A, B, C"),
+        }
+
+        //increase score for choice
+        match your_choice {
+            'X' => score += 1,
+            'Y' => score += 2,
+            'Z' => score += 3,
+            _ => panic!("unexpected value in second column, acceptable values are: X, Y, Z"),
+        }
+    }
+    score
+}

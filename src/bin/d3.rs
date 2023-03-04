03a2c6704064d1608ff7ec64e7dc48e04ef443a0
Jacob Neave <35272149+aircooledjacob@users.noreply.github.com>
2023-03-04 22:48:34 +0000

    complete d3
    created binary to solve day 3

---
+++ src/bin/d3.rs
@@ -0,0 +1,46 @@
+use std::collections::HashSet;
+
+use clap::Parser;
+
+use advent_of_code_2022::common;
+
+/// Simple cli tool to solve the third day of advent of code
+#[derive(Parser)]
+#[command(version)]
+struct Args {
+    /// Path to the input file
+    #[arg(short, long)]
+    filename: String,
+}
+
+fn main() {
+    let args = Args::parse();
+
+    let raw_lines: Vec<String> = common::read_file_to_vec_of_strings(&args.filename);
+
+    let mut sum_of_duplicate_priorities: u32 = 0;
+
+    for rucksack in raw_lines {
+        let (compartment_1, compartment_2) = rucksack.split_at(rucksack.len() / 2);
+
+        let duplicates: HashSet<char> = compartment_2
+            .chars()
+            .filter(|item| compartment_1.contains(*item))
+            .collect();
+
+        let duplicate_priorities: HashSet<u8> = duplicates
+            .iter()
+            .map(|duplicate| {
+                if *duplicate as u8 >= b'a' {
+                    (*duplicate as u8 - b'a') + 1
+                } else {
+                    (*duplicate as u8 - b'A') + 27
+                }
+            })
+            .collect();
+
+        sum_of_duplicate_priorities += u32::from(duplicate_priorities.iter().sum::<u8>());
+    }
+
+    println!("{sum_of_duplicate_priorities}")
+}
